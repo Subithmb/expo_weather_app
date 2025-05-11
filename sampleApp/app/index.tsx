6ecfa282389378433bd4e5faec30eeae2c1bb661
Subithmb <bijusubith@gmail.com>
2025-05-11 19:59:00 +0530

    expo app project stater file with nativewind

---
+++ sampleApp/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import "../global.css"
+
+export default function Index() {
+  return (
+    <View
+    className="flex-1 justify-center items-center bg-gray-400"
+    
+    >
+
+<Text className="text-blue-500 text-2xl font-bold">hello world !</Text>
+
+    </View>
+  );
+}
